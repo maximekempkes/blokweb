183d6a3cf92d5a382837c2783241bdfda3b6dbe5
maximekempkes <94305205+maximekempkes@users.noreply.github.com>
2021-11-25 17:07:16 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,3 @@
-// JavaScript Document
-Can be used with JavaScript
------------------------------
 const icnMenu = document.querySelector('.menu-icon');
 icnMenu.addEventListener('click', () => {
 	icnMenu.classList.toggle('active');
